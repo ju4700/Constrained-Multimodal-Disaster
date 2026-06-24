7f757a7be7f8665caf4e1bb4b42febdde6c296d2
ju4700 <jalal.dev.design@gmail.com>
2026-06-24 21:42:24 +0600

    Fix ColorJitter TypeError by changing 'value' to 'brightness'

--- phase1_benchmark.py
+++ phase1_benchmark.py
@@ -217,7 +217,7 @@ def run_ablation():
     
     train_transform = transforms.Compose([
         transforms.Resize((224, 224)),
-        transforms.ColorJitter(hue=0.2, saturation=0.2, value=0.2),
+        transforms.ColorJitter(brightness=0.2, hue=0.2, saturation=0.2),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
